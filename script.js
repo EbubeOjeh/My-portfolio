2a512660e8732531b04b5bcdc8b308dd99418958
Ebube Ojeh <ojehebube07@gmail.com>
2026-03-09 22:12:17 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal() {
+    document.getElementById("imageModal").style.display = "block";
+    document.getElementById("modalImage").src = "pic.jpeg";
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
